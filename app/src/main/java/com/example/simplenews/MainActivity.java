d55657fbc3f78c3f6f762235c36233170ace7a50
cliffordli <1833015726@qq.com>
2020-05-02 00:07:48 +0800

    添加主页面的fragment滑动响应

--- app/src/main/java/com/example/simplenews/MainActivity.java
+++ app/src/main/java/com/example/simplenews/MainActivity.java
@@ -23,10 +23,20 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener{
 
+    private ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +45,8 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
         setSupportActionBar(toolbar);
-        //侧边栏抽屉
+
+        /*侧边栏抽屉*/
         DrawerLayout drawer=findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         //drawer.addDrawerListener();
@@ -44,6 +55,46 @@ public class MainActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+
+        /*主页面选择*/
+        viewPager = findViewById(R.id.viewPager);
+        setupViewPager(viewPager);
+        /*当按下底部按钮时，改变显示页面*/
+        final BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavView);
+        bottomNavigationView.getMenu().getItem(1).setChecked(true);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                //viewPager.setVisibility(View.VISIBLE);
+                switch (menuItem.getItemId()) {
+                    case R.id.bottomNav_home:
+                        viewPager.setCurrentItem(0);
+                        return true;
+                    case R.id.bottomNav_my:
+                        viewPager.setCurrentItem(1);
+                        return true;
+                    default: break;
+                }
+                return false;
+            }
+        });
+        /*当滑动页面的时候改变底部按钮的显示*/
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {  //滑动页面后的效果
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
     @Override
@@ -98,5 +149,49 @@ public class MainActivity extends AppCompatActivity
                 .setMessage(message)
                 .setPositiveButton("确定", null)
                 .show();
+    }
+
+    public void setupViewPager(ViewPager viewPager) {
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        homeFragment homeFragment = new homeFragment();
+        myFragment myFragment = new myFragment();
+        viewPagerAdapter.addFragment(homeFragment);
+        viewPagerAdapter.addFragment(myFragment);
+        viewPager.setAdapter(viewPagerAdapter);
+        /*默认显示主页面*/
+        viewPager.setCurrentItem(1);
+    }
+}
+
+
+class ViewPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> fragmentList = new ArrayList<Fragment>();
+    private Fragment instantFragment;
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        fragmentList.add(fragment);
+    }
+
+    @Override
+    public void setPrimaryItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        instantFragment = (Fragment) object;
+        super.setPrimaryItem(container, position, object);
+    }
+    public Fragment getInstantFragment() {
+        return instantFragment;
     }
 }
